4675c99c48dd5723a742ee416510c4f18867f283
nat <nat.foster@gmail.com>
2019-09-20 15:09:51 -0600

    feat: post to slack

--- hpe-connector/handler.py
+++ hpe-connector/handler.py
@@ -21,12 +21,14 @@ def handle_sei_event(event, context):
     delivery_id = body['delivery_id']
     delivery_json = get_delivery_json(exam_id, delivery_id)
 
-    send_to_psi(delivery_json)
-    send_to_saba(delivery_json)
+    psi_response = send_to_psi(delivery_json)
+    saba_response = send_to_saba(delivery_json)
+
+    send_to_slack(delivery_json, psi_response, saba_response)
 
     response = {
         'statusCode': 200,
-        'body': json.dumps(delivery_json)
+        'body': '{}'
     }
 
     return response
@@ -41,17 +43,63 @@ def get_delivery_json(exam_id, delivery_id):
 
 
 def send_to_psi(delivery_json):
-    booking_code = 'CAFLH8DY'
+    examinee_info = delivery_json['examinee']['info']
+    booking_code = examinee_info.get('bookingcode')
+    score = delivery_json['score']
+    result = 'PASS' if delivery_json['passed'] else 'FAIL'
     payload = {
-      "result": "PASS",
-      "score": "90"
+      'result': result,
+      'score': score
     }
     url = '{}/assessmentResultService/1.0.0/bookings/{}/results'.format(os.environ['PSI_URL_BASE'], booking_code)
     r = requests.put(url, json=payload)
-    print(url)
-    print(r.status_code)
-    print(r.text)
+    return {'Status Code': r.status_code}
 
 
 def send_to_saba(delivery_json):
-    pass
+    return {'Status Code': None}
+
+
+def send_to_slack(delivery_json, psi_response, saba_response):
+    webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
+    if webhook_url:
+        channel = os.environ.get('SLACK_CHANNEL', '#dev')
+        examinee_attachment = {
+            'pretext': 'Someone has completed a delivery in the {0} exam'.format(delivery_json['exam']['name']),
+            'title': 'Examinee Info',
+            'text': '```{0}```'.format(
+                json.dumps(delivery_json['examinee']['info'], sort_keys=True, indent=4, separators=(',', ': '))),
+            'mrkdwn_in': [
+                'text'
+            ]
+        }
+        delivery_info = {
+            'score': delivery_json['score'],
+            'score_scale': delivery_json['score_scale'],
+            'passed': delivery_json['passed'],
+            'points_earned': delivery_json['points_earned'],
+            'points_available': delivery_json['points_available']
+        }
+
+        delivery_attachment = format_attachment('Delivery Info', delivery_info)
+        psi_attachment = format_attachment('PSI Status', psi_response)
+        saba_attachment = format_attachment('SABA Status', saba_response)
+
+        slack_payload = {
+            'username': 'HPE Connector',
+            'icon_emoji': ':owl:',
+            'channel': channel,
+            'attachments': [examinee_attachment, delivery_attachment, psi_attachment, saba_attachment]
+        }
+        requests.post(webhook_url, json=slack_payload)
+
+
+def format_attachment(title, data):
+    attachment = {
+        'title': title,
+        'text': '```{0}```'.format(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))),
+        'mrkdwn_in': [
+            'text'
+        ]
+    }
+    return attachment
