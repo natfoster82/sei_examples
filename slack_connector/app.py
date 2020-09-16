21d7f02e5458c721427a899d605017a864318aa5
nat <natfoster@gmail.com>
2020-09-16 15:40:08 -0600

    basic auth integration for slack connector

--- slack_connector/app.py
+++ slack_connector/app.py
@@ -26,19 +26,7 @@ csrf = CSRFProtect(app)
 
 # some helpers
 redis_store = StrictRedis.from_url(app.config['REDIS_URL'], db=app.config['REDIS_DB'], decode_responses=True)
-
-
-def get_integration_info(exam_id):
-    data = redis_store.get(exam_id)
-    if data:
-        return loads(data)
-    url = app.config['SEI_URL_BASE'] + '/api/integrations/' + exam_id + '/credentials'
-    resp = requests.get(url, auth=HTTPBasicAuth(username=app.config['SEI_ID'], password=app.config['SEI_SECRET']))
-    if resp.status_code != 200:
-        raise ValueError('No access to this exam_id')
-    data = resp.json()
-    redis_store.set(data['exam_id'], dumps(data))
-    return data
+basic_auth = HTTPBasicAuth(username=app.config['SEI_ID'], password=app.config['SEI_SECRET'])
 
 
 class ConfigureForm(FlaskForm):
@@ -67,7 +55,7 @@ def sei_redirect():
     if not confirm_token:
         abort(400)
     url = app.config['SEI_URL_BASE'] + '/api/integrations/confirm/' + confirm_token
-    resp = requests.get(url, auth=HTTPBasicAuth(username=app.config['SEI_ID'], password=app.config['SEI_SECRET']))
+    resp = requests.get(url, auth=basic_auth)
     if resp.status_code != 200:
         abort(400)
     data = resp.json()
@@ -93,19 +81,18 @@ def delivery_completed():
     # authorize the request
     body = request.get_json()
     exam_id = body['exam_id']
-    integration_info = get_integration_info(exam_id)
+    integration_info = redis_store.get(exam_id)
     auth_header = request.headers.get('Authorization')
     token = auth_header.split()[1]
     try:
-        decoded = jwt.decode(token, integration_info['secret'], algorithms=['HS256'])
+        decoded = jwt.decode(token, app.config['SEI_SECRET'], algorithms=['HS256'])
     except jwt.exceptions.InvalidTokenError:
         return jsonify(), 403
 
     # get full delivery object from SEI
     delivery_id = body['delivery_id']
     delivery_url = '{0}/api/exams/{1}/deliveries/{2}?include=exam'.format(app.config['SEI_URL_BASE'], exam_id, delivery_id)
-    delivery_headers = {'Authorization': 'Bearer {0}'.format(integration_info['token'])}
-    delivery_response = requests.get(delivery_url, headers=delivery_headers)
+    delivery_response = requests.get(delivery_url, auth=basic_auth)
     delivery_json = delivery_response.json()
 
     slack_webhook_url = integration_info.get('slack_webhook_url')
@@ -148,9 +135,8 @@ def delivery_widget():
     exam_id = request.args.get('exam_id')
     delivery_id = request.args.get('delivery_id')
     token = request.args.get('jwt')
-    integration_info = get_integration_info(exam_id)
     try:
-        decoded = jwt.decode(token, integration_info['secret'], algorithms=['HS256'])
+        decoded = jwt.decode(token, app.config['SEI_SECRET'], algorithms=['HS256'])
     except jwt.exceptions.InvalidTokenError:
         abort(403)
     return render_template('delivery_widget.html', exam_id=exam_id, delivery_id=delivery_id, token=token)
@@ -160,9 +146,9 @@ def delivery_widget():
 def configure():
     exam_id = request.args.get('exam_id')
     token = request.args.get('jwt')
-    integration_info = get_integration_info(exam_id)
+    integration_info = redis_store.get(exam_id)
     try:
-        decoded = jwt.decode(token, integration_info['secret'], algorithms=['HS256'])
+        decoded = jwt.decode(token, app.config['SEI_SECRET'], algorithms=['HS256'])
     except jwt.exceptions.InvalidTokenError:
         abort(403)
     form = ConfigureForm(**integration_info)
